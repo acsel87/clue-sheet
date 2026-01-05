095827e111405a73204a65f0d8a84f571623f2a8
acsel87 <40614434+acsel87@users.noreply.github.com>
2026-01-05 17:53:34 +0200

    test fix 1

--- src/main.tsx
+++ src/main.tsx
@@ -16,6 +16,15 @@ async function registerServiceWorker() {
   })
 }
 
+function setAppHeight() {
+  const doc = document.documentElement;
+  doc.style.setProperty('--app-height', `${window.innerHeight}px`);
+}
+
+window.addEventListener('resize', setAppHeight);
+window.addEventListener('orientationchange', setAppHeight);
+setAppHeight();
+
 registerServiceWorker()
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
